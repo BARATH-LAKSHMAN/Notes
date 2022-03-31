cf645f91355e6f1a9efd574da4f25e33174b6863
BARATH-LAKSHMAN <barathlakshman06@.com>
2022-03-31 11:57:58 +0530

    first commit

---
+++ build/precache-manifest.109303cff0cfd21e46194d770980c776.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c76101d3b61356b720d167b3ecd4069",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "8c78e3b80702cbbc256d",
+    "url": "/static/js/main.e8e2132e.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
